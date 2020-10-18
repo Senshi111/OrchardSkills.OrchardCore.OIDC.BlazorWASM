34b5ccbba0b06741551a7706037fd6e10c20f56e
senshi111 <banenicovic@gmail.com>
2020-10-18 17:01:31 +0200

    restore program.cs

--- OrchardSkills.OrchardCore.BlazorWASM/Program.cs
+++ OrchardSkills.OrchardCore.BlazorWASM/Program.cs
@@ -15,12 +15,26 @@ namespace BlazorWASM
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddHttpClient("api")
+                .AddHttpMessageHandler(sp =>
+                {
+                    var handler = sp.GetService<AuthorizationMessageHandler>()
+                        .ConfigureHandler(
+                            authorizedUrls: new[] { "https://localhost:44342" },
+                            scopes: new[] { "weatherapi" });
+
+                    return handler;
+                });
+
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("api"));
+
             builder.Services
                 .AddOidcAuthentication(options =>
                 {
                     builder.Configuration.Bind("oidc", options.ProviderOptions);
-                });
-
+                    options.UserOptions.RoleClaim = "role";
+                })
+                .AddAccountClaimsPrincipalFactory<ArrayClaimsPrincipalFactory<RemoteUserAccount>>();
 
             await builder.Build().RunAsync();
         }
